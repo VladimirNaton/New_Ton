2eaa52cd3a28421053b795575462c4d05e0f8e75
Владимир <volodymyr.tochylin@varus.ua>
2022-09-21 20:38:55 +0300

    Создал пакеты структуры проекта

--- src/main/java/com/new_ton/NewTonApplication.java
+++ src/main/java/com/new_ton/NewTonApplication.java
@@ -1,8 +1,10 @@
-package com.example.new_ton;
+package com.new_ton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class NewTonApplication {
 
